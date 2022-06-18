fc7c2e669315686e8635e2b2eed7c6b42be31bd6
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-06-18 14:01:52 -0300

    arrumando bug no carrossel

--- src/components/SecaoPrincipal.vue
+++ src/components/SecaoPrincipal.vue
@@ -1,15 +1,15 @@
 <template>
-    <section id="secaoPrincipal" class="mt-2 rounded p-0 text-white">
-        <div id="coverBg" class="carousel slide" data-bs-ride="true">
+    <section id="secaoPrincipal" class="mt-2 rounded p-0 px-1 text-white">
+        <div id="coverBg" class="carousel slide rounded" data-bs-ride="true">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#coverBg" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#coverBg" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#coverBg" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div class="carousel-inner">
+            <div class="carousel-inner rounded">
                 <div class="carousel-item active">
                     <img src="../assets/bg-header.jpg" class="d-block w-100" alt="...">
-                    <h4 class="text-center">Você precisa saber sobre a cotação das suas criptomoedas preferidas?</h4>
+                    <h4 class="text-center">Você quer saber sobre a cotação das suas criptomoedas preferidas?</h4>
                 </div>
                 <div class="carousel-item">
                     <img src="../assets/bg-header.jpg" class="d-block w-100" alt="...">
@@ -47,32 +47,42 @@ export default {
     @import '../assets/cores.scss';
 
     #secaoPrincipal {
-        background-image: url(../assets/bg-header.jpg);
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
         width: 100%;
-        height: 350px;
+        height: 300px;
 
         #coverBg {
             width: 100%;
-            height: 350px;
+            height: 300px;
             background-color: rgba($color1, .5);
             .carousel-inner {
                 background-color: $color1;
                 .carousel-item {
                     position: relative;
-                    height: 350px;
-                    align-items: center;
-                    justify-content: center;
-                    display: flex;
+                    height: 300px;
+                    background-color: $color1;
                    
                     h4 {
                         position: absolute;
                         margin: auto;
+                        font-weight: 400;
+                        width: 75%;
+                        z-index: 1;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0;
+                        display: flex;
+                        align-items: center;
+
+                        @media only screen and (max-width: 765px) {
+                            font-size: 1.1em;
+                        }
                     }
                     img {
-                        opacity: .2;
+                        width: 100%;
+                        height: 100%;
+                        object-fit: cover;
+                        opacity: .4;
                     }
                 }
             }
