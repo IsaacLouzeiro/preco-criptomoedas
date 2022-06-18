ec9988d14f959a8d6f70f788bb8a6e2cf7514f2e
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-06-18 15:51:11 -0300

    iniciando secao de principais moedas

--- src/components/SecaoPrincipal.vue
+++ src/components/SecaoPrincipal.vue
@@ -1,11 +1,19 @@
 <template>
-    <section id="secaoPrincipal" class="mt-2 rounded p-0 px-1">
+    <section id="secaoPrincipal" class="mt-2 rounded p-0">
         <!-- titulo -->
         <h2 class="text-center fw-bold mt-3">Principais moedas do mercado!</h2>
         <!-- principais moedas do mercado -->
-        <div class="row container mx-auto">
-            <div class="col-6 border"></div>
-            <div class="col-6 border"></div>
+        <div class="row container mx-auto g-1 justify-content-between px-2 moedasPrincipais">
+            <div class="col-md-6">
+                <section class="rounded">
+                    <span><i class="fa-brands fa-bitcoin"></i></span>
+                </section>
+            </div>
+            <div class="col-md-6">
+                <section class="rounded">
+                    <span><i class="fa-brands fa-ethereum"></i></span>
+                </section>
+            </div>
         </div>
     </section>
 </template>
@@ -26,5 +34,39 @@ export default {
 
     #secaoPrincipal {
         color: $color1;
+    }
+
+    .moedasPrincipais {
+        div {
+            section {
+                background-color: $color1;
+                padding: 10px;
+
+                span {
+                    width: 40px;
+                    height: 40px;
+                    border-radius: 20px;
+                    display: inline-block;
+                    background-color: rgba($color2, .9);
+                    justify-content: center;
+                    align-items: center;
+                    display: flex;
+    
+                    i {
+                        font-size: 1.4em;
+                        text-shadow: 0 0 5px $color2;
+    
+                        &.fa-ethereum {
+                            color: #3c3c3d;
+                        }
+    
+                        &.fa-bitcoin {
+                            color: #f7931a;
+                        }
+                    }
+                }
+            }
+
+        }
     }
 </style>
