dfe679a09d2e1e55f0120bc4dce1d76f07ca1772
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-06-19 16:42:43 -0300

    dando acesso a api

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-  headers: {
-    "X-CMC_PRO_API_KEY": "16514700-50e6-4f13-8564-8705d12cc15b"
-  }
+    baseURL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "https://criptomoeda.netlify.app",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        "X-CMC_PRO_API_KEY": "16514700-50e6-4f13-8564-8705d12cc15b"
+    }
 });
 
 export default api;
