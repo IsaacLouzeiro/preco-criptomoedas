253c6b004bff7273a8621060384cd57067428267
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-06-19 16:48:49 -0300

    alterando origin

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ const api = axios.create({
     baseURL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
     headers: {
         "Access-Control-Allow-Headers" : "criptomoeda",
-        "Access-Control-Allow-Origin": "https://criptomoeda.netlify.app",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
         "X-CMC_PRO_API_KEY": "16514700-50e6-4f13-8564-8705d12cc15b"
     }
 });
